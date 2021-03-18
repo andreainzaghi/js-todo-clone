bd13693cb86d742240fd36f0d239fa7cbc818e00
Andrea <andrea3aprile99@gmail.com>
2021-03-18 16:16:07 +0100

    inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var listaSpesa = [
+	'comprare vino',
+  'comprare pollo',
+  'comprare carte',
+];
+
+for (var i = 0; i < listaSpesa.length; i++) {
+	// clono il template
+	var template =$('template li').clone();
+	// aggiungo le info al template
+	template.prepend(listaSpesa[i]);
+	// aggiung ol iil template alla lista
+	$(this).parent().remove();
+}
+
+// cancello gli elementi dalla listaSpesa
+$('.todo-list__delete').click(function (){
+	$(this).parent().remove();
+});
+
+$('aggiungi').keydown(function (){
+	if(evento.which == 13){
+		var testo = $(this).val();
+		if (testo!=''){
+			// clono il template
+			var template =$('.template li').clone();
+			// aggiungo le info al template
+			 template.prepend(testo);
+			// aggiungo il template alla lista
+			 $('.todo-list').append(template);
+			 $(this).val('');
+		}
+	}
+});
+
+$(document).on('click','todo-list__delete',function(){
+	$(this).parent().remove();
+}
+);
