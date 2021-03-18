9ae5061957b4ebebcf9fbd2c9a8b7bf84fd254af
Andrea <andrea3aprile99@gmail.com>
2021-03-18 17:56:26 +0100

    m

--- js/script.js
+++ js/script.js
@@ -6,19 +6,14 @@ var listaSpesa = [
 
 for (var i = 0; i < listaSpesa.length; i++) {
 	// clono il template
-	var template =$('template li').clone();
+	var template =$('.template li').clone();
 	// aggiungo le info al template
 	template.prepend(listaSpesa[i]);
 	// aggiung ol iil template alla lista
-	$(this).parent().remove();
+  $(".todo-list").append(template);
 }
 
-// cancello gli elementi dalla listaSpesa
-$('.todo-list__delete').click(function (){
-	$(this).parent().remove();
-});
-
-$('#aggiungi').keydown(function (){
+$('#aggiungi').keydown(function (evento){
 	if(evento.which == 13){
 		var testo = $(this).val();
 		if (testo!=''){
@@ -33,7 +28,13 @@ $('#aggiungi').keydown(function (){
 	}
 });
 
-$(document).on('click','todo-list__delete',function(){
-	$(this).parent().remove();
+$(document).on('click','.todo-list__delete',function(){
+   var questo = this;
+	 $(".ter").removeClass("alert")
+	setTimeout(function () {
+  	 $(questo).parent().remove();
+
+	}, 10000 , questo);
+
 }
 );
